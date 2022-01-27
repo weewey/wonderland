636f0cacc29c0ffc1ba6599b4e28956b4c0adaa5
Varick Lim <varicklsr@gmail.com>
2022-01-27 21:50:19 +0800

    Copy game elements to concert

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -5,4 +5,5 @@ public class Constants
     public static String LoginScene = "Login";
     public static String CityScene = "City";
     public static String LobbyScene = "Lobby";
+    public static String ConcertScene = "Concert";
 }
