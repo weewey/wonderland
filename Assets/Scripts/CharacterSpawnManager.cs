0c8bd87ca2241ad5b635f6dd58ad79a72364be87
yewwee <ychua@thoughtworks.com>
2022-02-21 16:54:22 +0800

    add more characters to the game

--- Assets/Scripts/CharacterSpawnManager.cs
+++ Assets/Scripts/CharacterSpawnManager.cs
@@ -15,11 +15,22 @@ public class CharacterSpawnManager : SpawnManagerBase
 
     [SerializeField]
     protected GameObject m_Character2;
+    
+    [SerializeField]
+    protected GameObject m_Character3;
 
+    [SerializeField]
+    protected GameObject m_Character4;
+    
     private readonly PlayFabAuthService _authService = PlayFabAuthService.Instance;
 
     private const string Wallet1 = "4Lz2j3Ga6NeNfiUWfu3FakkVppcfuLsAYMLwq2ELbL6B";
+    
     private const string Wallet2 = "Eqi3aPp3EdTAs9puh4Ze4E5mJ2onzSSr9F94uuZz95qX";
+    
+    private const string Wallet3 = "EGuJ8jdrQKyXnSWgZqa2SLFWfzPyPEMeorsZ4GjdejqH";
+    
+    private const string Wallet4 = "V1KjqXRw1Dku3xAEDtAxyNUUcttF3WPDgX6gX9z1Vj2";
 
     /// <summary>
     /// Abstract method that allows for a character to be spawned based on the game logic.
@@ -34,9 +45,13 @@ public class CharacterSpawnManager : SpawnManagerBase
                 return m_Character1;
             case Wallet2:
                 return m_Character2;
+            case Wallet3:
+                return m_Character3;
+            case Wallet4:
+                return m_Character4;
         }
 
         // Default
-        return m_Character2;
+        return m_Character1;
     }
 }
